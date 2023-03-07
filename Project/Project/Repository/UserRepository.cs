803dfda4e17d41f841388ad44cf35a1ee1ce968d
Lazar Ristic <zrpgd023@gmail.com>
2023-03-07 10:57:14 -0800

    Created initial project

---
+++ Project/Project/Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Model;
+using Project.Serializer;
+
+namespace Project.Repository
+{
+    public class UserRepository
+    {
+
+        private const string FilePath = "../../../Resources/Data/users.csv";
+
+        private readonly Serializer<User> _serializer;
+
+        private List<User> _users;
+
+        public UserRepository()
+        {
+            _serializer = new Serializer<User>();
+            _users = _serializer.FromCSV(FilePath);
+        }
+
+        public User GetByUsername(string username)
+        {
+            _users = _serializer.FromCSV(FilePath);
+            return _users.FirstOrDefault(u => u.Username == username);
+        }
+
+    }
+}
---
+++ Project/Project/View/SignInView.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using Project.Repository;
+using Project.Model;
+using Project.View;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project.View
+{
+    /// <summary>
+    /// Interaction logic for SignInView.xaml
+    /// </summary>
+    public partial class SignInView : Window
+    {
+        private readonly UserRepository _repository;
+
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if (value != _username)
+                {
+                    _username = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public SignInView()
+        {
+            InitializeComponent();
+            DataContext = this;
+            _repository = new UserRepository();
+        }
+
+        private void SignIn(object sender, RoutedEventArgs e)
+        {
+            User user = _repository.GetByUsername(Username);
+            if (user != null)
+            {
+                if (user.Password == txtPassword.Password)
+                {
+                    /*CommentsOverview commentsOverview = new CommentsOverview(user);
+                    commentsOverview.Show();
+                    Close();*/
+                }
+                else
+                {
+                    MessageBox.Show("Wrong password!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wrong username!");
+            }
+
+        }
+    }
+}
