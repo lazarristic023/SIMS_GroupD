31dc2d8339d7e30e05a40cbdc2ac8a7f27c1ec46
Lazar Ristic <zrpgd023@gmail.com>
2023-03-10 14:51:12 -0800

    started accommodation filter

---
+++ Project/Project/Controller/Guest1Controller.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Model;
+using Project.Repository;
+
+namespace Project.Controller
+{
+    public class Guest1Controller
+    {
+        public Guest1 Guest { get; set; }
+        public AccommodationRepository AccommodationRepository { get; set; }
+        public AccommodationReservationRepository AccReservationRepository { get; set; }
+
+        public List<Location> AccommodationLocations { get; set; }
+        public ImageRepository ImageRepository { get; set; }
+
+        public Guest1Controller()
+        {
+            Guest = new Guest1();
+            AccommodationRepository = new AccommodationRepository();
+            AccReservationRepository = new AccommodationReservationRepository();
+            ImageRepository = new ImageRepository();
+            AccommodationLocations = new List<Location>();
+            LinkAccommodationsAndImages();
+            LinkGuest1Reservation();
+            FillAccommodationLocationsList();
+        }
+
+
+        public Guest1Controller(User u)
+        {
+            Guest = new Guest1(u);
+            AccommodationRepository = new AccommodationRepository();
+            AccReservationRepository = new AccommodationReservationRepository();
+            ImageRepository = new ImageRepository();
+            AccommodationLocations = new List<Location>();
+            LinkAccommodationsAndImages();
+            LinkGuest1Reservation();
+            FillAccommodationLocationsList();
+
+        }
+
+        private void LinkGuest1Reservation()
+        {
+            foreach (var reservation in AccReservationRepository.GetAllReservations())
+            {
+                if (reservation.GuestId == Guest.User.Id)
+                {
+                    Guest.Reservations.Add(reservation);
+                }
+            }
+        }
+
+        public List<AccommodationReservation> GetAccommodationReservations()
+        {
+            return Guest.Reservations;
+        }
+        public List<Accommodation> GetAccommodations()
+        {
+            return AccommodationRepository.GetAllAccommodations();
+        }
+
+        public List<Location> GetAccommodationLocations()
+        {
+            return AccommodationLocations;
+        }
+            
+        private void LinkAccommodationsAndImages()
+        {
+            foreach (var image in ImageRepository.GetAllImages())
+            {
+                Accommodation accommodation = AccommodationRepository.GetAllAccommodations().Find(a => a.Id == image.EntityId);
+                if (accommodation == null)
+                {
+                    continue;
+                }
+                if (accommodation.Images.Exists(i => i.Id == image.Id))
+                {
+                    continue;
+                }
+
+                accommodation.Images.Add(image);
+            }
+        }
+
+        private void FillAccommodationLocationsList()
+        {
+            foreach(var accommodation in AccommodationRepository.GetAllAccommodations())
+            {
+                Location location = 
+                    AccommodationLocations.Find(l => (l.City == accommodation.Location.City) && (l.Country == accommodation.Location.Country));
+
+                if (location == null)
+                {
+                    AccommodationLocations.Add(accommodation.Location);
+                }
+            }
+
+        }
+
+
+    }
+}
--- Project/Project/View/Guest1View/Guest1View.xaml.cs
+++ Project/Project/View/Guest1View/Guest1View.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Project.Controller;
+using Project.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +22,57 @@ namespace Project.View
     /// </summary>
     public partial class Guest1View : Window
     {
-        public Guest1View()
+        private Guest1Controller controller;
+        private User user;
+        public ObservableCollection<AccommodationReservation> Reservations { get; set; }
+        public ObservableCollection<Accommodation> Accommodations { get; set; }
+
+        public ObservableCollection<string> Countries { get; set; }
+        public ObservableCollection<string> CountryCities { get; set; }
+
+        public string SelectedCountry { get; set; }
+        public string SelectedCity { get; set; }
+        public Guest1View(User u)
         {
             InitializeComponent();
+            DataContext = this;
+            controller = new Guest1Controller(u);
+            Reservations = new ObservableCollection<AccommodationReservation>(controller.GetAccommodationReservations());
+            Accommodations = new ObservableCollection<Accommodation>(controller.GetAccommodations());
+            Countries = new ObservableCollection<string>();
+            CountryCities = new ObservableCollection<string>();
+            FillCountriesList();
+
+        }
+
+        private void btSignOut_Click(object sender, RoutedEventArgs e)
+        {
+            SignInView signInView = new SignInView();
+            Close();
+            signInView.Show();
+        }
+
+        private void FillCountriesList()
+        {
+            foreach (var location in controller.GetAccommodationLocations())
+            {
+                if (!Countries.Contains(location.Country))
+                {
+                    Countries.Add(location.Country);
+                }
+            }
+        }
+
+        private void cbCountry_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CountryCities.Clear();
+            foreach (var location in controller.GetAccommodationLocations())
+            {
+                if (location.Country == SelectedCountry)
+                {
+                    CountryCities.Add(location.City);
+                }
+            }
         }
     }
 }
