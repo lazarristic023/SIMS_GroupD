95009134306c79fe7d76699e49cfe22114b2e13a
Luka Rakin <rakin.ra205.2020@uns.ac.rs>
2023-03-13 07:27:47 +0100

    Added classes: Tour, TourPoint, TourRepository, TourPointRepository

---
+++ Project/Project/Model/TourPoint.cs
@@ -0,0 +1,57 @@
+﻿using Project.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Project.Model
+{
+    public class TourPoint: ISerializable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public bool Action { get; set; }
+
+        public TourPoint()
+        {
+            Id = -1;
+            Name = "";
+            Order = 0;
+            Action = false;
+
+        }
+
+        public TourPoint(int id, string name, int order, bool action, int tourId)
+        {
+            Id = id;
+            Name = name;
+            Order = order;
+            Action = action;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = {
+                Id.ToString(),
+                Name,
+                Order.ToString(),
+                Action.ToString()
+               
+            };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = int.Parse(values[0]);
+            Name = values[1];
+            Order = int.Parse(values[2]);
+            Action = bool.Parse(values[3]);
+
+        }
+    }
+
+}
---
+++ Project/Project/Repository/TourPointRepository.cs
@@ -0,0 +1,66 @@
+﻿using Project.Model;
+using Project.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Project.Repository
+{
+    public class TourPointRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tourpoint.csv";
+
+        private readonly Serializer<TourPoint> serializer;
+
+        private List<TourPoint> tourPoints;
+
+        public TourPointRepository()
+        {
+            serializer = new Serializer<TourPoint>();
+            tourPoints = serializer.FromCSV(FilePath);
+        }
+
+        private void SaveInFile()
+        {
+            serializer.ToCSV(FilePath, tourPoints);
+        }
+
+        private int GenerateId()
+        {
+            if (tourPoints.Count == 0)
+                return 0;
+
+            return tourPoints[tourPoints.Count - 1].Id + 1;
+        }
+
+        public void Add(TourPoint tourPoint)
+        {
+            tourPoint.Id = GenerateId();
+            tourPoints.Add(tourPoint);
+            SaveInFile();
+
+        }
+
+        public void Remove(int id)
+        {
+            TourPoint tourPoint = GetById(id);
+
+            tourPoints.Remove(tourPoint);
+            SaveInFile();
+
+        }
+
+        public TourPoint GetById(int id)
+        {
+            return tourPoints.Find(v => v.Id == id);
+        }
+
+        public List<TourPoint> GetAll()
+        {
+            return tourPoints;
+        }
+    }
+}
