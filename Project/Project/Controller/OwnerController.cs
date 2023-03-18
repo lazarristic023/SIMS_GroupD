ba144d0e8aa7d168651bd5cc69900f45e831d79e
Ognjen Milicevic <ognjen@infocentar.rs>
2023-03-18 01:09:06 +0100

    my accommodations tab in owner's view

---
+++ Project/Project/Controller/OwnerController.cs
@@ -0,0 +1,40 @@
+﻿using Project.Model;
+using Project.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Controller
+{
+    public class OwnerController
+    {
+        public Owner Owner { get; set; }
+        public AccommodationRepository AccommodationRepository { get; set; }
+
+        public OwnerController()
+        {
+            Owner = new Owner();
+            AccommodationRepository = new AccommodationRepository();
+            LinkOwnerAccommodation();
+        }
+        public OwnerController(User u)
+        {
+            Owner = new Owner(u);
+            AccommodationRepository = new AccommodationRepository();
+            LinkOwnerAccommodation();
+        }
+        private void LinkOwnerAccommodation()
+        {
+            foreach(Accommodation accommodation in AccommodationRepository.GetAllAccommodations())
+            {
+                if(accommodation.OwnerId == Owner.User.Id)
+                {
+                    Owner.Accommodations.Add(accommodation);
+                }
+            }
+        }
+
+    }
+}
