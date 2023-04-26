5cdffddc80536fd0e3cca8aed2c0d60c8c004b33
luka-rakin <116868040+luka-rakin@users.noreply.github.com>
2023-04-26 20:37:40 +0200

    TourRequest view

---
+++ Project/Project/View/TourGuideView/TourRequests.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project.View.TourGuideView
+{
+    /// <summary>
+    /// Interaction logic for TourRequests.xaml
+    /// </summary>
+    public partial class TourRequests : Window
+    {
+        public TourRequests()
+        {
+            InitializeComponent();
+        }
+    }
+}
